323f623cfce1c56c367d40ff025eeabe81008dd2
Marco Ramirez <marco.ramirez.a@hotmail.com>
2023-03-17 18:49:37 -0500

    Objects Labs

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: 'marco',
+    streetAdress: "123 Sesame ST"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   return {
+    ...employee,
+    [key]: value,
+   }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+employee[key] = value
+return employee
+}
+function deleteFromEmployeeByKey(employee,key) {
+    const newEmployee ={employee}
+    employee[key] = "Sam"
+    return newEmployee
+}function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
